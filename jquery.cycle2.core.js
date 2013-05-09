046d69796c06d8c04d9c263c17af0c1ce4c3127b
malsup <malsup@gmail.com>
2013-05-09 18:30:03 -0400

    #186, fix timeout issue with first slide

--- jquery.cycle2.core.js
+++ jquery.cycle2.core.js
@@ -138,7 +138,7 @@ $.fn.cycle.API = {
         // stage initial transition
         if ( opts.timeout ) {
             slideOpts = opts.API.getSlideOpts( opts.nextSlide );
-            opts.API.queueTransition( slideOpts, opts.timeout + opts.delay );
+            opts.API.queueTransition( slideOpts, slideOpts.timeout + opts.delay );
         }
 
         opts._initialized = true;
