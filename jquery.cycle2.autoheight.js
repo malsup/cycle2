958a04a05c08b97ee838c1ac8e36852f668bd4c5
Nico Amarilla <amarillanico@gmail.com>
2013-09-13 18:08:44 +0800

    Added autoHeightSpeed and autoHeightEasing to Cycle2 options. Used when autoHeight is set to "container"

--- jquery.cycle2.autoheight.js
+++ jquery.cycle2.autoheight.js
@@ -1,9 +1,11 @@
-/*! Cycle2 autoheight plugin; Copyright (c) M.Alsup, 2012; version: 20130304 */
+/*! Cycle2 autoheight plugin; Copyright (c) M.Alsup, 2012; version: 20130913 */
 (function($) {
 "use strict";
 
 $.extend($.fn.cycle.defaults, {
-    autoHeight: 0 // setting this option to false disables autoHeight logic
+    autoHeight: 0, // setting this option to false disables autoHeight logic
+    autoHeightSpeed: 250,
+    autoHeightEasing: null
 });    
 
 $(document).on( 'cycle-initialized', function( e, opts ) {
