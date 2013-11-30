a67c5242d56f7b71c8fc61ffe6e8b24ae81d8822
malsup <malsup@gmail.com>
2013-11-30 11:30:24 -0500

    tCycle changes consistent with logic of pull #235 (https://github.com/malsup/cycle2/pull/235)

--- jquery.tcycle.js
+++ jquery.tcycle.js
@@ -1,38 +1,37 @@
-/*! tCycle (c) 2012 M.Alsup MIT/GPL 20120911 */
+/*! tCycle (c) 2013 M.Alsup MIT/GPL 20131130 */
 (function($){
 "use strict";
 $.fn.tcycle = function(){
 
 return this.each(function(){
-	var f, i=0, c=$(this), s=c.children(), o=$.extend({speed:500,timeout:4000},c.data()),
-		l=s.length, w=c.width(), z=o.speed, t=o.timeout;
-	c.prepend($(s[0]).clone().css('visibility','hidden')).css({position:'relative',overflow:'hidden'});
-	f=o.fx!='scroll';
+	var i=0, c=$(this), s=c.children(), o=$.extend({speed:500,timeout:4000},c.data()), f=o.fx!='scroll',
+		l=s.length, w=c.width(), z=o.speed, t=o.timeout, css={overflow:'hidden'}, p='position', a='absolute',
+		tfn=function(){setTimeout(tx,t)}, scss = $.extend({position:a,top:0}, f?{left:0}:{left:w}, o.scss);
+	if (c.css(p)=='static')
+		css[p]='relative';
+	c.prepend($(s[0]).clone().css('visibility','hidden')).css(css);
+	s.css(scss);
 	if(f)
-		s.css({position:'absolute',top:0,left:0}).hide().eq(0).show();
+		s.hide().eq(0).show();
 	else
-		s.css({position:'absolute',top:0,left:w}).eq(0).css('left',0);
+		s.eq(0).css('left',0);
 	setTimeout(tx,t);
 
 	function tx(){
 		var n = i==(l-1) ? 0 : (i+1), w=c.width(), a=$(s[i]), b=$(s[n]);
 		if (f){
 			a.fadeOut(z);
-			b.fadeIn(z,function(){
-				setTimeout(tx,t);
-			});
+			b.fadeIn(z,tfn);
 		}else{
 			a.animate({left:-w},z,function(){
 				a.hide();
 			});
-			b.css({'left':w,display:'block'}).animate({left:0},z,function(){
-				setTimeout(tx,t);
-			});
+			b.css({'left':w,display:'block'}).animate({left:0},z,tfn);
 		}
 		i = i==(l-1) ? 0 : (i+1);
 	}
 });
 
 };
-$(document).ready(function(){$('.tcycle').tcycle();});
+$(function(){$('.tcycle').tcycle()});
 })(jQuery);
